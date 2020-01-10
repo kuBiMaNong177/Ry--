c6a1876b1c4c2adca4acf52a20922bc5be85a3e8
Renshuaikang <1596739628@qq.com>
2020-01-10 22:44:54 +0800

    提交了one4个小路由

--- src/components/footer/index.vue
+++ src/components/footer/index.vue
@@ -1,7 +1,7 @@
 <template>
 
-     <footer class="bot">
-      <router-link class="active" :to='{name:"one"}'>
+   <footer class="bot">
+      <router-link class="aa" :to='{name:"one"}'>
         <span class="iconfont icon-gedan"></span>
         <em>发现音乐</em>
       </router-link>
@@ -17,28 +17,45 @@
         <span class="iconfont icon-wode"></span>
         <em>账号</em>
       </router-link>
-
-   </footer>
+    </footer>
 </template>
 
 <script>
 export default {
-  name:'Footer',
-  data(){
-    return {
-      active: 0
-    }
-  },
-}
+  name: "Footer"
+};
 </script>
 
 <style scoped>
 /* footer */
-.main .bot{border-top: 1px #ccc solid; display: flex;justify-content: space-around;align-items: center;height:3rem;width:100%;}
-.main .bot a {display: flex;flex-direction: column;align-items: center;color: rgba(170, 170, 170, 1);}
-.main .bot .active{color:#de635c;}
-.main .bot a span{font-size: 1.25rem;}
-.main .bot a em{font-size: 0.84rem;}
-
-
+.footer_bot{
+  height: 3rem;
+  width: 100%;
+}
+.main .bot {
+  border-top: 1px #ccc solid;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  height: 3rem;
+  width: 100%;
+  position: fixed;
+  bottom: 0px;
+  background-color: rgba(255, 255, 255, 1);
+}
+.main .bot a {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  color: rgba(170, 170, 170, 1);
+}
+.main .bot .aa {
+  color: rgba(0, 0, 0, 1);
+}
+.main .bot a span {
+  font-size: 1.25rem;
+}
+.main .bot a em {
+  font-size: 0.84rem;
+}
 </style>
